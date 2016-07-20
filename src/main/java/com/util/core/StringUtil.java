1095440e573f4ed44ffd6e2574de0d2c3b81325e
tank2140896 <631094182@qq.com>
2016-07-20 20:29:37 +0800

    init

---
+++ src/main/java/com/util/core/StringUtil.java
@@ -0,0 +1,13 @@
+package com.util.core;
+
+import java.util.Optional;
+
+public class StringUtil{
+	
+	//处理字符串的null值,如果为null返回空
+	public static String handleNullString(String str) {
+		Optional<String> optional = Optional.ofNullable(str);
+		return optional.map(String::toString).orElse("");
+	}
+
+}
