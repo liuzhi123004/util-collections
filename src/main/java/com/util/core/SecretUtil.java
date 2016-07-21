c38635163cf6c0a867c3dbcde52151452c130621
MrRiver <631094182@qq.com>
2016-07-21 17:54:14 +0800

    Create SecretUtil.java

---
+++ src/main/java/com/util/core/SecretUtil.java
@@ -0,0 +1,12 @@
+package com.util.core;
+
+import java.util.UUID;
+
+public class SecretUtil {
+	
+	//获取UUID
+	public static String getRandomUUID(){
+		return UUID.randomUUID().toString();
+	}
+    
+}
