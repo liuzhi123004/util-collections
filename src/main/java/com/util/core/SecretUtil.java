4e5e415e7953d37c7cefd4be94f4fb05b7620106
MrRiver <631094182@qq.com>
2016-08-01 10:12:38 +0800

    Update SecretUtil.java

--- src/main/java/com/util/core/SecretUtil.java
+++ src/main/java/com/util/core/SecretUtil.java
@@ -16,22 +16,35 @@ public class SecretUtil {
 	
 	private static final int CHINESE_USERD_LENGTH = CHINESE_USED_END-CHINESE_USED_START;
 	
-	//默认字符集  
-	public static final char[] CHARACTERSET = new char[KEYBORD_USERD_LENGTH+CHINESE_USERD_LENGTH];
+	public static final char[] KEYBORD_USED_CHARACTERSET = new char[KEYBORD_USERD_LENGTH];
+	
+	public static final char[] CHINESE_USED_CHARACTERSET = new char[KEYBORD_USERD_LENGTH+CHINESE_USERD_LENGTH];
 
 	static{
 		int count = 0;
 		for (int i = KEYBORD_USERD_START; i < KEYBORD_USERD_END; i++) {
-			CHARACTERSET[count++] = (char)i;
+			KEYBORD_USED_CHARACTERSET[count] = (char)i;
+			CHINESE_USED_CHARACTERSET[count] = (char)i;
+			count++;
 		}
 		for (int i = CHINESE_USED_START; i < CHINESE_USED_END; i++) {
-			CHARACTERSET[count++] = (char)i;
+			CHINESE_USED_CHARACTERSET[count++] = (char)i;
 		}
 	}
 
 	//获取UUID
 	public static String getRandomUUID(){
 		return UUID.randomUUID().toString();
+	}
+	
+	//默认生成随机密码
+	public static String defaultGenRandomPass(int passLen){
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < passLen; i++) {
+			int randomNum = (int)(Math.random()*KEYBORD_USERD_LENGTH);
+			sb.append(KEYBORD_USED_CHARACTERSET[randomNum]);
+		}
+		return sb.toString();
 	}
 	
 }
